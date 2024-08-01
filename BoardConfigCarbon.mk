47cdc38747eab8073e1332f017c3389ea7a84e2c
mommaimfine30 <155714908+mommaimfine30@users.noreply.github.com>
2024-08-01 11:33:47 -0400

    update

--- BoardConfigCarbon.mk
+++
@@ -1,9 +0,0 @@
-# Custom board config for taimen
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-
--include vendor/google/taimen/BoardConfigVendor.mk
